a61ca9e5a72f23cb6978552807f899bae62c41f3
Ash-1453 <agerald1@terpmail.umd.edu>
2025-10-02 17:39:00 -0400

    Add files via upload

--- lab5.py
+++ lab5.py
@@ -4,8 +4,9 @@ import math
 GPIO.setmode(GPIO.BCM)
 pin=[14,15,18,23,24,25,8,7,12,16]
 pwm=[]
-b=[]
+brightness=[]
 y=0
+phi=math.pi/11
 
 for x in pin:
 	GPIO.setup(x, GPIO.OUT, initial=0)
@@ -19,7 +20,7 @@ try:
 		y=0
 		for x in pin:
 			z=time.time()
-			b.append(100*(math.sin(2*math.pi*.2*z-((math.pi*y)/11)))^2)
+			b.append(100*(math.sin(math.pi*.2*2*z-phi))**2)
 			pwm[y].ChangeDutyCycle(b[y])
 			y+=1
 except KeyboardInterrupt: # if user hits ctrl-C
